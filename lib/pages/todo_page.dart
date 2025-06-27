ba1ae6c9e1a830b9541c62fcfbf491531dce6070
roacode <vns5218@icloud.com>
2025-06-27 08:10:34 -0300

    first commit

---
+++ lib/pages/todo_page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:state_flutter/components/todo_tile.dart';
+import 'package:state_flutter/data/database.dart';
+import 'package:state_flutter/util/dialog_box.dart';
+
+class TodoPage extends StatefulWidget {
+  const TodoPage({super.key});
+
+  @override
+  State<TodoPage> createState() => _TodoPageState();
+}
+
+class _TodoPageState extends State<TodoPage> {
+  final _controller = TextEditingController();
+
+  final _myBox = Hive.box('mybox');
+  TodoDataBase db = TodoDataBase();
+
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      db.toDoList[index][1] = value;
+    });
+    db.updateDataBase();
+  }
+
+  void checkBoxDelete(int index) {
+    setState(() {
+      db.toDoList.removeAt(index);
+    });
+    db.updateDataBase();
+  }
+
+  void saveNewTask() {
+    setState(() {
+      db.toDoList.add([_controller.text, false]);
+    });
+    Navigator.of(context).pop();
+    _controller.text = "";
+    db.updateDataBase();
+  }
+
+  void createNewTask() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return DialogBox(
+          controller: _controller,
+          onCancel: () => Navigator.of(context).pop(),
+          onSave: saveNewTask,
+        );
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    // if this is the 1st time ever opening the app, then create default data
+    if (_myBox.get("TODOLIST") == null) {
+      db.createInitialData();
+    } else {
+      db.loadData();
+    }
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        backgroundColor: Colors.yellow,
+        title: Center(child: Text('TO DO')),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewTask,
+        backgroundColor: Colors.yellow,
+        child: Icon(Icons.add, color: Colors.black),
+      ),
+      body: ListView.builder(
+        itemCount: db.toDoList.length,
+        itemBuilder: (context, index) {
+          return TodoTile(
+            taskName: db.toDoList[index][0],
+            taskCompleted: db.toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index),
+            handleDelete: (context) => checkBoxDelete(index),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/util/dialog_box.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:state_flutter/components/my_button.dart';
+
+class DialogBox extends StatelessWidget {
+  final TextEditingController? controller;
+  final VoidCallback onSave;
+  final VoidCallback onCancel;
+  const DialogBox({
+    super.key,
+    required this.controller,
+    required this.onSave,
+    required this.onCancel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.yellow[300],
+      content: SizedBox(
+        height: 120,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.yellow),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                hintText: 'Add a new task',
+              ),
+            ),
+
+            Row(
+              spacing: 10,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MyButton(onPressed: onCancel, text: 'Cancel'),
+                MyButton(onPressed: onSave, text: 'Save'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
